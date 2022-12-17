db745c36ca7fbc6d3320d30f8deac2764b580f0a
Texterly <88582978+Texterly@users.noreply.github.com>
2022-12-17 15:06:32 +0300

    added eslint and NodeList

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NodeList from './components/NodeList';
+
+function App() {
+  return (
+    <div className="container">
+      <NodeList />
+    </div>
+  );
+}
+
+export default App;
