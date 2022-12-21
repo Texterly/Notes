e2aaefdc353392e2df1004a3bcf5b1d05241a373
Texterly <88582978+Texterly@users.noreply.github.com>
2022-12-21 20:40:46 +0300

    added Dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import NodeList from './components/NodeList';
 import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
   const [notes, setNotes] = useState([{
@@ -22,6 +23,8 @@ function App() {
 
   const [searchText, setSearchText] = useState('');
 
+  const [darkMode, setDarkMode] = useState(false);
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -39,13 +42,16 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Search handleSearchNote={setSearchText} />
-      <NodeList
-        notes={notes.filter((note) => note.text.toLocaleLowerCase().includes(searchText))}
-        handleAddNote={addNote}
-        handleDeleteNote={deleteNote}
-      />
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NodeList
+          notes={notes.filter((note) => note.text.toLocaleLowerCase().includes(searchText))}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
 }
